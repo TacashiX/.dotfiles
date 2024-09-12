cfca22b7d044a149b619d7c2b4fcce9b66d4e892
tacashix <tacashix@gmail.com>
2024-09-12 17:30:06 +0200

    fixed double launch

--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -1,7 +1,11 @@
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
-  done
-else
-  polybar --reload example &
-fi
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload example &
+#   done
+# else
+#   polybar --reload example &
+# fi
+
+#!/bin/sh
+MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
+MONITOR=$MONITORS polybar --reload example &
