3b6055dedfa83c69b0741c3e7da455b3d16299ec
TacashiX <tacashix@gmail.com>
2025-05-31 22:34:51 +0200

    stuff

--- misc/clipboard_sync/clipboard_sync_server.sh
+++ misc/clipboard_sync/clipboard_sync_server.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
-# Config
-REMOTE_IP="$1"         # IP of the peer (e.g., 192.168.122.2)
-LOCAL_PORT="$2"        # Port to listen on
-REMOTE_PORT="$3"       # Port to send to
+# Arguments
+REMOTE_IP="$1"
+LOCAL_PORT="$2"
+REMOTE_PORT="$3"
 
-# Init last hash
+# Dependencies check
+for cmd in wl-paste wl-copy socat sha256sum; do
+    command -v "$cmd" >/dev/null || {
+        echo "Missing dependency: $cmd"
+        exit 1
+    }
+done
+
+# Temp files
 LAST_HASH=""
+TMP_DIR=$(mktemp -d)
+trap "rm -rf $TMP_DIR" EXIT
 
-# Watch clipboard and send if new
+# Watch clipboard loop
 watch_clipboard() {
-  wl-paste --watch --progress --foreground --type text/plain,image/png --watch-cmd - | \
-  while IFS= read -r line || [[ -n "$line" ]]; do
-    TEMP_FILE=$(mktemp)
-    wl-paste --type text/plain,image/png > "$TEMP_FILE"
+    while true; do
+        TEMP_FILE="$TMP_DIR/clip.dat"
 
-    NEW_HASH=$(sha256sum "$TEMP_FILE" | cut -d ' ' -f1)
+        # Dump clipboard to temp file
+        wl-paste --no-newline > "$TEMP_FILE" 2>/dev/null || continue
+        NEW_HASH=$(sha256sum "$TEMP_FILE" | cut -d ' ' -f1)
 
-    if [[ "$NEW_HASH" != "$LAST_HASH" ]]; then
-      socat -u "OPEN:$TEMP_FILE,rdonly" TCP:$REMOTE_IP:$REMOTE_PORT
-      LAST_HASH="$NEW_HASH"
-    fi
+        if [[ "$NEW_HASH" != "$LAST_HASH" ]]; then
+            socat -u "OPEN:$TEMP_FILE,rdonly" TCP:$REMOTE_IP:$REMOTE_PORT
+            LAST_HASH="$NEW_HASH"
+        fi
 
-    rm "$TEMP_FILE"
-  done
+        sleep 0.3
+    done
 }
 
-# Receive incoming clipboard data and apply
+# Clipboard receiver
 receive_clipboard() {
-  socat -u TCP-LISTEN:$LOCAL_PORT,reuseaddr,fork SYSTEM:'wl-copy --type text/plain,image/png'
+    socat -u TCP-LISTEN:$LOCAL_PORT,reuseaddr,fork SYSTEM:'wl-copy'
 }
 
-# Start both in parallel
+# Run both in parallel
 watch_clipboard &
 receive_clipboard
 
